54764e5a334e0f1abe0a1e1a70eb90cc94d9770f
Jay3ee <Jibola422@gmail.com>
2020-11-04 11:47:37 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+   var cha =  document.querySelector(".speedyQuadsView");
+   var chap =  document.querySelector(".info");
+document.getElementById("yellow").addEventListener("click", function(){
+     
+    cha.style.backgroundColor = "#FBEC06";
+})
+    
+
+    var yel =  document.querySelector(".speedyQuadsView");
+    var yelo =  document.querySelector(".info");
+ document.getElementById("bb").addEventListener("click", function(){
+      
+     yelo.style.backgroundColor = "yellow";
+ })
+ 
+ /*   
+   // document.getElementById("model").style.color = "black";
+   // document.getElementById("sn").style.color = "black";
+    document.getElementsByTagName("p").style.color = "black";
+});
+
+document.getElementById("blue").addEventListener("click", function(){
+     
+    cha.style.backgroundColor = "#0392BF";
+});
+
+document.getElementById("pink").addEventListener("click", function(){
+     
+    cha.style.backgroundColor = "#ED48BA";
+});
+
+document.getElementById("gray").addEventListener("click", function(){
+    cha.style.backgroundColor = "#B3D4E0";
+});
+*/
